f2431b620e891cb3484da86210ed872f63496e92
Tom Ritchford <tom@swirly.com>
2023-04-11 11:29:37 +0200

    Add clsprop.py

---
+++ clsprop.py
@@ -0,0 +1,20 @@
+import sys
+
+
+class clsprop(property):
+    """
+    Just like property but for class objects.
+
+    From https://stackoverflow.com/a/39542816/43839
+    """
+    def __get__(self, obj, objtype=None):
+        return super(clsprop, self).__get__(objtype)
+
+    def __set__(self, obj, value):
+        super(clsprop, self).__set__(type(obj), value)
+
+    def __delete__(self, obj):
+        super(clsprop, self).__delete__(type(obj))
+
+
+sys.modules[__name__] = clsprop
